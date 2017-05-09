b3a583f68e008ed901ac2c74b767334e8b37abca
i7657043 <i7657043@bournemouth.ac.uk>
2017-05-09 09:20:53 +0100

    Jordan Commit
    Updated sql tables, addition of full plan to repo

---
+++ create_tables_script.sql
@@ -0,0 +1,30 @@
+CREATE TABLE tbl_readings(
+id int not null AUTO_INCREMENT,
+device_temp VARCHAR(3),
+ambient_temp VARCHAR(3),
+internal_temp VARCHAR(3),
+lumens VARCHAR(3),
+decibels VARCHAR(3),
+status VARCHAR(3),
+battery VARCHAR(3),
+timestamp timestamp default currrent_timestamp not null,
+primary key (id)
+);
+
+
+
+CREATE TABLE tbl_device(
+id int not null AUTO_INCREMENT,
+device_name VARCHAR(30),
+first_online timestamp not null default current_timestamp,
+last_online timestamp not null default current_timestamp on update current_timestamp,
+owner VARCHAR(30),
+device_temp VARCHAR(3),
+ambient_temp VARCHAR(3),
+internal_temp VARCHAR(3),
+lumens VARCHAR(3),
+decibels VARCHAR(3),
+status VARCHAR(3),
+battery VARCHAR(3),
+primary key (id)
+);
